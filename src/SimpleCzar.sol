3d4bf229e7081e29f1414751b509794f32982fb8
malteish <malteish@corpus.io>
2023-07-06 08:32:34 +0200

    add SimpleCzar

---
+++ src/SimpleCzar.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// DssVest - Token vesting contract
+//
+// Copyright (C) 2021 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+abstract contract ERC20 {
+    function approve(address, uint256) external virtual returns (bool);
+}
+
+/**
+ * @title SimpleCzar
+ * @author malteish
+ * @notice This contract is used to hold tokens that can only be transferred by the DssVestTransferrable contract.
+ */
+contract SimpleCzar is Initializable {
+    constructor(ERC20 _token, address _DssVestTransferrable) {
+        initialize(_token, _DssVestTransferrable);
+    }
+
+    function initialize(ERC20 _token, address _DssVestTransferrable) public initializer {
+        _token.approve(_DssVestTransferrable, type(uint256).max);
+    }
+}
