cfe2f7a5362a6af03888bd8d092bb5bec2fbb148
malteish <malteish@corpus.io>
2023-07-06 09:19:27 +0200

    clone SimpleCzar and Transferrable in one tx

--- src/SimpleCzarCloneFactory.sol
+++ src/SimpleCzarCloneFactory.sol
@@ -13,10 +13,23 @@ contract SimpleCzarCloneFactory is DssVestCloneFactory {
 
     constructor(address _implementation) DssVestCloneFactory(_implementation) {}
 
+
+
     function createSimpleCzarClone(bytes32 salt, ERC20 gem, address dssVestTransferrable) external returns (address) {
         address clone = Clones.cloneDeterministic(implementation, salt);
         SimpleCzar(clone).initialize(gem, dssVestTransferrable);
         emit NewClone(clone);
         return clone;
+    }
+
+    function createSimpleCzarAndTransferrableClone(bytes32 salt, ERC20 gem, DssVestTransferrableCloneFactory dssVestTransferrableCloneFactory, address ward) external returns (address) {
+        address transferrableCloneAddress = dssVestTransferrableCloneFactory.predictCloneAddress(salt);
+        
+        // create and initialize the SimpleCzar clone
+        address simpleCzarClone = Clones.cloneDeterministic(implementation, salt);
+        SimpleCzar(simpleCzarClone).initialize(gem, transferrableCloneAddress);
+
+        // create and initialize the DssVestTransferrable clone
+        return dssVestTransferrableCloneFactory.createTransferrableVestingClone(salt, simpleCzarClone, address(gem), ward);
     }
 }
